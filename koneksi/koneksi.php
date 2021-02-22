d42120d65300535f063fdc2b30934b5589eb71c6
dzocrusher05 <prayudid6@gmail.com>
2021-02-23 01:02:09 +0800

    Add files via upload

---
+++ koneksi/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$koneksi = mysqli_connect("localhost","root","","silpa");
+
+// check konesi
+if (mysqli_connect_errno()){
+  echo "Koneksi Database Gagal : " . mysqli_connect_errno();
+}
+
+?>
